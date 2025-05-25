c7239962ae93afc741251c207c593ca9046fd5ea
github-ci <you@example.com>
2025-05-25 17:19:51 +0000

    test(bacon-qr-code-bundle): 注释掉集成测试中的多余配置

--- tests/Integration/IntegrationTestKernel.php
+++ tests/Integration/IntegrationTestKernel.php
@@ -42,9 +42,9 @@ class IntegrationTestKernel extends Kernel
             'php_errors' => [
                 'log' => true,
             ],
-            'validation' => [
-                'email_validation_mode' => 'html5',
-            ],
+//            'validation' => [
+//                'email_validation_mode' => 'html5',
+//            ],
 //            'uid' => [
 //                'default_uuid_version' => 7,
 //                'time_based_uuid_version' => 7,
